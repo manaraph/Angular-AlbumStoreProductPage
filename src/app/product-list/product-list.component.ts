1f7c6744375ba658e252ea70809694d9975f2977
manaraph <manaraph225@gmail.com>
2019-10-25 02:45:08 +0100

    Complete part 6

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from '../product';
-import { ProductService } from 'app/product.service';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-product-list',
@@ -8,11 +8,12 @@ import { ProductService } from 'app/product.service';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
-  product: Product[];
+  products: Product[];
 
   constructor(private _productService: ProductService) { }
 
   ngOnInit() {
+    this._productService.getProducts().subscribe( response => this.products = response );
   }
 
 }
