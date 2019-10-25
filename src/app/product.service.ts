d45910878135aabddc22d9912e61dc043aeb2385
manaraph <manaraph225@gmail.com>
2019-10-25 02:27:08 +0100

    generate product-list component and product interface

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http, Response } from "@angular/http";
+import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 import { Observable } from 'rxjs/Observable';
 import { Album } from './album';
